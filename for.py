4768d606cc75ce8b391f63f9bc3b83df6bb2aa3d
Thomas Hsu <th30845@gmail.com>
2019-09-20 15:21:35 +0800

    For loop

---
+++ for.py
@@ -0,0 +1,14 @@
+#For loop learning
+cars = ['Toyota', 'Honda']
+
+for car in cars:  #bring Toyota & Honda out
+	print(car)
+
+names = ['Thomas', 'Jakc', 'Tom']
+for name in names:
+	print(name)
+
+#exercise
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for student in students:
+	print('Hi', student)
